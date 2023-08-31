d4f6331c1450adc44c1bbd75d2cb16210bf1959d
Anton Kozyriev <sonara4mus@gmail.com>
2023-08-31 20:10:28 +0300

    feat(src.pipeline): add check if file to partition exists

--- src/pipeline/repartition.py
+++ src/pipeline/repartition.py
@@ -1,26 +1,45 @@
 import argparse
+import logging
+import pathlib
+import sys
 
 from pyspark.sql import SparkSession
 
-parser = argparse.ArgumentParser(description="Util to repartition *.JSON lines file into chunks for faster processing.")
 
-parser.add_argument("--input", type=str, default="../.data/raw.json", help="A path to datafile to repartiton.")
-parser.add_argument("--output", type=str, default="../.data/dataframe", help="A path to output repartitioned dataset.")
-parser.add_argument("--partitions", type=int, default=48, help="Number of partitions in the output.")
+def main():
+    logging.basicConfig(level=logging.INFO, format="(%(asctime)s) [%(levelname)s]: %(message)s.")
+    logger = logging.getLogger()
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Util to repartition *.JSON lines file into chunks for faster processing.")
 
-spark = SparkSession.builder \
-    .appName("Steam reviews dataset: Repartition script")\
-    .master("local[*]")\
-    .config("spark.driver.memory","45G")\
-    .config("spark.driver.maxResultSize", "0")\
-    .config("spark.kryoserializer.buffer.max", "2000M")\
-    .getOrCreate()
+    parser.add_argument("--input", type=str, default="../.data/raw.json", help="A path to datafile to repartiton.")
+    parser.add_argument("--output", type=str, default="../.data/dataframe", help="A path to output repartitioned dataset.")
+    parser.add_argument("--partitions", type=int, default=48, help="Number of partitions in the output.")
+
+    args = parser.parse_args()
+
+    spark = SparkSession.builder \
+        .appName("Steam reviews dataset: Repartition script")\
+        .master("local[*]")\
+        .config("spark.driver.memory","45G")\
+        .config("spark.driver.maxResultSize", "0")\
+        .config("spark.kryoserializer.buffer.max", "2000M")\
+        .getOrCreate()
+
+    if not pathlib.Path(args.input).is_file():
+        logger.error(f"A datafile path does not exist: `{args.input}`")
+        sys.exit(1)
+
+    logger.info(f"Partitioning datafile `{args.input}` into `{args.partitions}` chunks")
+
+    df = spark.read.format("json").option("lines", "true").load(args.input)
+    df.coalesce(args.partitions).write.format("json").mode("overwrite").save(args.output)
+
+    logger.info(f"Partitions successfully recorded into directory `{args.output}`")
+
+    spark.stop()
 
 
-df = spark.read.format("json").option("lines", "true").load(args.input)
-df.coalesce(args.partitions).write.format("json").mode("overwrite").save(args.output)
-
-spark.stop()
+if __name__ == "__main__":
+    main()
 
