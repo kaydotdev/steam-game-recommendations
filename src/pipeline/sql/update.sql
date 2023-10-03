ba6a421b151900f17b96dee9970bec3b24ea7930
Anton Kozyriev <sonara4mus@gmail.com>
2023-10-03 15:53:46 +0300

    feat(src.pipeline): load tables script with ignoring updates

--- src/pipeline/sql/update.sql
+++ src/pipeline/sql/update.sql
@@ -10,19 +10,38 @@ INSERT INTO public."Users" (userprofile, products)
 SELECT userprofile, products
 FROM public."UsersAppend"
 ON CONFLICT (userprofile)
-DO NOTHING;
+DO UPDATE SET
+    products = EXCLUDED.products;
 
 INSERT INTO public."Games" (app_id, title, description, tags, date_release, rating, positive_ratio, user_reviews, price_final, price_original, discount, win, mac, linux, steam_deck)
 SELECT app_id, title, description, tags, date_release, rating, positive_ratio, user_reviews, price_final, price_original, discount, win, mac, linux, steam_deck
 FROM public."GamesAppend"
 ON CONFLICT (app_id)
-DO NOTHING;
+DO UPDATE SET
+    title = EXCLUDED.title,
+    description = EXCLUDED.description,
+    tags = EXCLUDED.tags,
+    date_release = EXCLUDED.date_release,
+    rating = EXCLUDED.rating,
+    positive_ratio = EXCLUDED.positive_ratio,
+    user_reviews = EXCLUDED.user_reviews,
+    price_final = EXCLUDED.price_final,
+    price_original = EXCLUDED.price_original,
+    discount = EXCLUDED.discount,
+    win = EXCLUDED.win,
+    mac = EXCLUDED.mac,
+    linux = EXCLUDED.linux,
+    steam_deck = EXCLUDED.steam_deck;
 
 INSERT INTO public."Recommendations" (app_id, userprofile, date, is_recommended, helpful, funny, hours)
 SELECT app_id, userprofile, date, is_recommended, helpful, funny, hours
 FROM public."RecommendationsAppend"
 ON CONFLICT (app_id, userprofile, date)
-DO NOTHING;
+DO UPDATE SET
+    is_recommended = EXCLUDED.is_recommended,
+    helpful = EXCLUDED.helpful,
+    funny = EXCLUDED.funny,
+    hours = EXCLUDED.hours;
 
 -- Removing temporary `append` tables
 DROP TABLE IF EXISTS public."UsersAppend";
